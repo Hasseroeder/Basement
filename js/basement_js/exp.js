761c2fa3c974d6af6300be15663a6f8075e23afa
Hasse <148720088+Hasseroeder@users.noreply.github.com>
2025-05-02 20:56:56 +0000

    Create exp.js

---
+++ js/basement_js/exp.js
@@ -0,0 +1,78 @@
+const streakWorthButton = document.getElementById("streakWorthButton");
+const streakWorthContainer = document.getElementById("streakWorthContainer");
+let isstreakWorthCreated = false;
+
+const globalButton = document.getElementById("globalButton");
+const globalContainer = document.getElementById("globalContainer");
+let isglobalCreated = false;
+
+
+streakWorthButton.addEventListener('click', async function() {
+    if (isstreakWorthCreated) {
+        // Delete the <div> if it exists
+        const divToRemove = document.querySelector(".dynamic-streak-div");
+        if (divToRemove) {
+            streakWorthContainer.removeChild(divToRemove);
+        }
+        isstreakWorthCreated = false; // Update the flag
+    }else{ 
+      try {
+          const response = await fetch('donatorPages/streakExp.html'); 
+          if (!response.ok) {
+              throw new Error('Failed to fetch the file');
+          }
+          const htmlContent = await response.text();
+  
+          // Create a div and add the fetched content
+          const newDiv = document.createElement('div');
+          newDiv.innerHTML = htmlContent;
+          newDiv.className = "dynamic-streak-div";
+        
+          // Append the div to the container
+          document.getElementById('streakWorthContainer').appendChild(newDiv);
+          
+          MathJax.typeset();
+          isstreakWorthCreated = true;
+          
+      } catch (error) {
+          console.error('Error:', error);
+      }
+    }  
+});
+
+globalButton.addEventListener('click', async function() {
+    if (isglobalCreated) {
+        // Delete the <div> if it exists
+        const divToRemove = document.querySelector(".dynamic-global-div");
+        if (divToRemove) {
+            globalContainer.removeChild(divToRemove);
+        }
+        isglobalCreated = false; // Update the flag
+    }else{ 
+      try {
+          const response = await fetch('donatorPages/globalExp.html'); 
+          if (!response.ok) {
+              throw new Error('Failed to fetch the file');
+          }
+          const htmlContent = await response.text();
+  
+          // Create a div and add the fetched content
+          const newDiv = document.createElement('div');
+          newDiv.innerHTML = htmlContent;
+          newDiv.className = "dynamic-global-div";
+        
+          // Append the div to the container
+          document.getElementById('globalContainer').appendChild(newDiv);
+          
+          MathJax.typeset();
+          isglobalCreated = true;
+          
+      } catch (error) {
+          console.error('Error:', error);
+      }
+    }  
+});
+
+function copyMedian(){
+  navigator.clipboard.writeText("neonmath \nmedian(x) = log(2)/log(x/(x-1));");
+}
