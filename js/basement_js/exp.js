bd04b071d16b89c3b5499bb4ed3f8ad0c9d9d72c
Hasse <148720088+Hasseroeder@users.noreply.github.com>
2025-05-03 23:05:44 +0200

    Update exp.js

--- js/basement_js/exp.js
+++ js/basement_js/exp.js
@@ -6,6 +6,10 @@ const globalButton = document.getElementById("globalButton");
 const globalContainer = document.getElementById("globalContainer");
 let isglobalCreated = false;
 
+const knowledgeButton = document.getElementById("knowledgeButton");
+const knowledgeContainer = document.getElementById("knowledgeContainer");
+let isknowledgeCreated = false;
+
 
 streakWorthButton.addEventListener('click', async function() {
     if (isstreakWorthCreated) {
@@ -72,6 +76,39 @@ globalButton.addEventListener('click', async function() {
       }
     }  
 });
+
+knowledgeButton.addEventListener('click', async function() {
+    if (isknowledgeCreated) {
+        // Delete the <div> if it exists
+        const divToRemove = document.querySelector(".dynamic-knowledge-div");
+        if (divToRemove) {
+            knowledgeContainer.removeChild(divToRemove);
+        }
+        isknowledgeCreated = false; // Update the flag
+    }else{ 
+      try {
+          const response = await fetch('donatorPages/knowledge.html'); 
+          if (!response.ok) {
+              throw new Error('Failed to fetch the file');
+          }
+          const htmlContent = await response.text();
+  
+          // Create a div and add the fetched content
+          const newDiv = document.createElement('div');
+          newDiv.innerHTML = htmlContent;
+          newDiv.className = "dynamic-knowledge-div";
+        
+          // Append the div to the container
+          document.getElementById('knowledgeContainer').appendChild(newDiv);
+          
+          isknowledgeCreated = true;
+          
+      } catch (error) {
+          console.error('Error:', error);
+      }
+    }  
+});
+
 
 function copyMedian(){
   navigator.clipboard.writeText("neonmath \nmedian(x) = log(2)/log(x/(x-1));");
