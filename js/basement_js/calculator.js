098c8f9a3ccdaca14c3b065a348a6849b422c9fa
Mc Master <danielmertins0@gmail.com>
2025-06-04 00:12:02 +0200

    working on calculator

--- js/basement_js/calculator.js
+++ js/basement_js/calculator.js
@@ -6,9 +6,158 @@ function showTimestamps() {
     document.getElementById("timestamp2").textContent = formattedTime;
 }
 
+const neonURL = "https://neonutil.vercel.app/zoo-stats?s=4.1.10.1.1.3";
+
+const inputLvl = document.getElementById("inputLvl");
+const sliderLvl = document.getElementById("sliderLvl");
+const inputs = Array.from({ length: 6 }, (_, i) => document.getElementById(`input${i + 1}`));
+const outputs = Array.from({ length: 10 }, (_, i) => document.getElementById(`output${i + 1}`));
+
+let level = 0;
+
+const stats = [
+    0, //hp
+    0, //wp
+    0, //str
+    0, //mag
+    0, //pr
+    0  //mr
+    // order is differently here from otherwise, because team stat display is rotated 
+    // I'm using team stat display as reference here, instead of pet stat display
+]
+
+const internalStats = [
+    0, //hp
+    0, //wp
+    0, //str
+    0, //mag
+    0, //iPr
+    0  //iMr
+]
+
+const outsideStats = [
+    0, //hp i=0
+    0, //wp i=1
+    0, //str i=2
+    0, //mag i=3
+    0, //pr i=4
+    0, //mr i=5
+    0, //(pr)ehp i=6
+    0, //(mr)ehp i=7
+    0, //iPR i=8
+    0  //iMR i=9
+]
+
+// for outputs 1-4 & 9-10, use a multiplier and base stat amount
+// for outputs 5-6, use 9-10
+// for outputs 7-8, use 1+Res
+
+function lookForMatchingPets(){
+
+    //fetch('https://cors-anywhere.herokuapp.com/https://neonutil.vercel.app/zoo-stats?s=4.1.10.1.1.3')
+    //.then(response => console.log(response))
+    //.catch(error => console.error('Error fetching data:', error));
+
+
+    //fetch('https://cors-anywhere.herokuapp.com/https://neonutil.vercel.app/zoo-stats?s=4.1.10.1.1.3')
+    //.then(response => response.json())
+    //.then(data => console.log(data))
+    //.catch(error => console.error('Error fetching data:', error));
+
+    fetch("https://neonutil.vercel.app/zoo-stats?s=4.1.10.1.1.3") 
+    .then(response =>console.log(response)) 
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+
+function updateInternalStats(){
+    const base =[500,500,100,100,25,25]
+    const multi =[2,2,1,1,2,2]
+
+    for (let i = 0; i < internalStats.length; i++) {
+        internalStats[i] = base[i] + multi[i] * stats[i] * level;
+    }
+
+    updateOutsideStats();
+}
+
+function updateOutsideStats(){
+    outsideStats.forEach((_,i)=>{
+        if ( i<=3 ){
+            outsideStats[i]=internalStats[i];
+        }else if (i<=5){
+            outsideStats[i]=  0.8*internalStats[i]
+                / (100+internalStats[i]);
+        }else if (i<=7){
+            outsideStats[i]=   internalStats[0]
+                / (1-outsideStats[i-2]);
+        }else {
+            outsideStats[i]=internalStats[i-4];
+        }
+    })
+    updateOutputs();
+}
+
+function updateOutputs(){
+    outputs.forEach((output,i )=>{    
+        
+        let temp;
+        if (i==4 || i ==5){
+            temp= ((outsideStats[i]*100).toFixed(1)).toLocaleString()+"%"; 
+        }else{
+            temp= Math.round(outsideStats[i]).toLocaleString();
+        }
+        
+        output.textContent=temp;       
+    });
+}
+
+//start of event listener functions
+function updateStats(){
+    inputs.forEach((input,i) => {
+        stats[i]=input?.value;
+    });
+    updateInternalStats();
+}
+
+function updateLevelFromNumber(){
+
+    level = inputLvl.value;
+    sliderLvl.value=level;
+    updateInternalStats();
+}
+
+function updateLevelFromSlider(){
+    level = sliderLvl.value;
+    inputLvl.value=level;
+    updateInternalStats();
+}
+//end of event listener functions
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    
+
     showTimestamps();
 
-}
-);
+    inputs.forEach((input,i)=>{
+        input.addEventListener("change", function(){    
+            if (event.isTrusted) {
+                updateStats();
+            }
+        })
+    });
+    inputLvl.addEventListener("change", function(){
+        if (event.isTrusted) {
+            updateLevelFromNumber();
+        }
+    });
+    sliderLvl.addEventListener("input", function(){
+        if (event.isTrusted) {
+            updateLevelFromSlider();
+        }
+    });
+
+    lookForMatchingPets();
+    inputs[0].focus();
+});
