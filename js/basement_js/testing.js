cbb0b41ecf3e34fdec74ad012006cd3df422c9b2
Mc Master <danielmertins0@gmail.com>
2025-06-08 21:36:45 +0200

    debug statement

--- js/basement_js/testing.js
+++ js/basement_js/testing.js
@@ -103,6 +103,8 @@ function onInput(e) {
             suggestedPets.push(tempArray[i]);
         })
 
+        console.log("suggested pet: " + suggestedPets[0]);
+        console.log("chosen pet: " + chosenPet);
         if (suggestedPets[0] == chosenPet){
             console.log("hiding suggestions!");
             return hideSuggestions();
