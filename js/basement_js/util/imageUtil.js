2c8ce265018e6b5bd5729bc83d67ea95acd6aca9
Mc Master <danielmertins0@gmail.com>
2025-09-09 13:27:58 +0200

    minor refactor for readability

--- js/basement_js/util/imageUtil.js
+++ js/basement_js/util/imageUtil.js
@@ -16,19 +16,22 @@ export function smallInjector({
 		const link = `/${baseHref}.html#${idOffset+id}`;
 		const path =`media/owo_images/f_${object.at(-1).toLowerCase()}.png`;
 		const text = object[0] + (object[1]? "<br>"+object[1]:"");
-		appendImageLink(container,link,path,text);
+		container.append(
+			getImageLink(link,path,text)
+		);
 	});
 
 	if (includeCalculator){
-		appendImageLink(container,"/weaponcalculator.html", "media/misc_images/cogwheel2.png", "Weapon Calculator");
+		container.append(
+			getImageLink("/weaponcalculator.html", "media/misc_images/cogwheel2.png", "Weapon Calculator")
+		);
 	}
 }
 
-function appendImageLink(container, link, path, text){
+function getImageLink(link, path, text){
 	const a = document.createElement("a");
 	a.href = link;
 	a.className = "tooltip";
-	container.append(a);
 
 	const img = document.createElement("img");
 	img.src = path;
@@ -39,4 +42,6 @@ function appendImageLink(container, link, path, text){
 	tooltip.innerHTML = text;
 	tooltip.className = "navBar-tooltip-text";
 	a.append(tooltip);
+
+	return a;
 }
