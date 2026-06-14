7761aa1623b778f464d5b055929eb9c9261607cf
Heather <148720088+Hasseroeder@users.noreply.github.com>
2026-06-14 20:37:17 +0200

    fixed differentiation between hb and zoo

--- huntbot/calculator/calculator.js
+++ huntbot/calculator/calculator.js
@@ -693,7 +693,7 @@ function displayZoo() {
 }
 
 function processPet({ callOrigin, tierPets, pet, petCaught, digitsNeeded, row, cell }) {
-	const visible = pet.caught.zoo !== 0
+	const visible = pet.caught[callOrigin] !== 0
 	if (visible && !row._visibility) {
 		row.style.display = 'flex'
 		row._visibility = true
